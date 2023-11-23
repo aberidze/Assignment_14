c2c09c7a3d937d9dcb651b674f42f5ec06885142
Anri_Beridze <aberidze02@gmail.com>
2023-11-24 02:10:42 +0400

    Project Cleaned

--- Assignment_14/SceneDelegate.swift
+++ Assignment_14/SceneDelegate.swift
@@ -14,11 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
-        guard let someScene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(windowScene: someScene)
-        let homeVC = HomeViewController()
-        window?.rootViewController = UINavigationController(rootViewController: homeVC)
-        window?.makeKeyAndVisible()
+        guard let _ = (scene as? UIWindowScene) else { return }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
